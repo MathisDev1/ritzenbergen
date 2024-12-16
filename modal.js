ee1c8c2d8c8cc4f959cb2bd3a51dc4951f5c5d83
BuildTools <unconfigured@null.spigotmc.org>
2024-12-16 08:25:10 +0100

    Modal bulitipp.php Navbar repariert

--- modal.js
+++ modal.js
@@ -1,6 +1,11 @@
 document.querySelectorAll(".modal-container").forEach((element)=>{
     if(element.querySelector(".openBtn")==null){
-
+        document.getElementById(element.dataset.id).addEventListener("click",(ev)=>{
+            var btn=ev.target;
+            var modal=document.getElementById(btn.dataset.id);
+            var content=modal.querySelector(".modal");
+            content.style.display="block";
+        });
     }else{
     element.querySelector(".openBtn").addEventListener("click",(ev)=>{
         var btn=ev.target;
